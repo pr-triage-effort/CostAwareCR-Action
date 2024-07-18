eb3cef136defff04d725e28e511110c3814f2255
Maxim Luchiacniuc <76953276+makasim98@users.noreply.github.com>
2024-07-18 01:02:59 -0400

    fix to reviewer review count error

--- src/features/features_reviewer.py
+++ src/features/features_reviewer.py
@@ -3,9 +3,10 @@ from datetime import datetime, timedelta, timezone
 from github import Github
 from github.PullRequest import PullRequest
 from github.NamedUser import NamedUser
+from github.Repository import Repository
 
 from features.features_author import author_features
-from features.user_utils import is_bot_user, try_get_reviews_num
+from features.user_utils import is_bot_user, is_user_reviewer, try_get_reviews_num
 
 DAYS_PER_YEAR = 365.25
 TIME_WINDOW_DAYS = 60
@@ -36,7 +37,7 @@ def reviewer_features(pr: PullRequest, api: Github, cache: dict):
             # Experience
             total_reviewer_experience += get_reviewer_experience(pr, reviewer, user_cache)
             # Review count
-            total_reviewer_review_num += user_cache.get(reviewer_name, {}).get('author_review_number')
+            total_reviewer_review_num += get_reviewer_review_cnt(reviewer, user_cache, api)
 
     # Reviewer features for posted reviews
     for review in reviews:
@@ -53,7 +54,7 @@ def reviewer_features(pr: PullRequest, api: Github, cache: dict):
             # Experience
             total_reviewer_experience += get_reviewer_experience(pr, reviewer, user_cache)
             # Review count
-            total_reviewer_review_num += user_cache.get(reviewer_name, {}).get('author_review_number')
+            total_reviewer_review_num += get_reviewer_review_cnt(reviewer, user_cache, api)
 
     # Compute reviewer features
     avg_reviewer_experience = 0
@@ -94,7 +95,7 @@ def get_reviewer_experience(pr: PullRequest, user: NamedUser, user_cache: dict) 
 
     return experience
 
-def get_reviewer_review_cnt(user: NamedUser, user_cache: dict, api: Github) -> int:
+def get_reviewer_review_cnt(user: NamedUser, repo: Repository, user_cache: dict, api: Github) -> int:
     username = user.login
 
     # Try retrieve from cache
@@ -108,12 +109,21 @@ def get_reviewer_review_cnt(user: NamedUser, user_cache: dict, api: Github) -> i
     start_date = now - timedelta(days=TIME_WINDOW_DAYS)
     reviews = try_get_reviews_num(username, start_date, now, api)
 
+    if reviews is None:
+        reviews = 0
+        prs = repo.get_pulls(state='closed')
+        for pr in prs:
+            if pr.closed_at < user.created_at:
+                break
+
+            if is_user_reviewer(pr, user):
+                reviews += 1
+
     # Cache result
-    if reviews is not None:
-        if user not in user_cache:
-            user_cache[username] = {
-                'author_review_number': reviews
-            }
+    if user not in user_cache:
+        user_cache[username] = {
+            'author_review_number': reviews
+        }
     else:
         user_cache[username]['author_review_number'] = reviews
 
