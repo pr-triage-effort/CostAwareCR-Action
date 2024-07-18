26315267fe3ea1f54002493ddb933d8203c5de7d
Maxim Luchiacniuc <76953276+makasim98@users.noreply.github.com>
2024-07-18 00:22:38 -0400

    Reduce request num when getting reviewer features

--- src/features/features_reviewer.py
+++ src/features/features_reviewer.py
@@ -1,12 +1,18 @@
+from datetime import datetime, timedelta, timezone
+
 from github import Github
 from github.PullRequest import PullRequest
+from github.NamedUser import NamedUser
 
 from features.features_author import author_features
-from features.user_utils import is_bot_user
+from features.user_utils import is_bot_user, try_get_reviews_num
+
+DAYS_PER_YEAR = 365.25
+TIME_WINDOW_DAYS = 60
 
 def reviewer_features(pr: PullRequest, api: Github, cache: dict):
     # Cached author data
-    reviewer_cache = cache.get('users', {})
+    user_cache = cache.get('users', {})
 
     # Temp data
     requested_reviewers = pr.requested_reviewers
@@ -28,16 +34,9 @@ def reviewer_features(pr: PullRequest, api: Github, cache: dict):
             human_reviewers += 1
 
             # Experience
-            reviewer_experience = reviewer_cache.get(reviewer_name, {}).get('author_experience', None)
-            if reviewer_experience is not None:
-                total_reviewer_experience += reviewer_experience
-            else:
-                # Compute user data
-                author_features(pr, api, cache, diff_user=reviewer)
-                total_reviewer_experience += reviewer_cache.get(reviewer_name, {}).get('author_experience')
-
+            total_reviewer_experience += get_reviewer_experience(pr, reviewer, user_cache)
             # Review count
-            total_reviewer_review_num += reviewer_cache.get(reviewer_name, {}).get('author_review_number')
+            total_reviewer_review_num += user_cache.get(reviewer_name, {}).get('author_review_number')
 
     # Reviewer features for posted reviews
     for review in reviews:
@@ -52,16 +51,9 @@ def reviewer_features(pr: PullRequest, api: Github, cache: dict):
             human_reviewers += 1
 
             # Experience
-            reviewer_experience = reviewer_cache.get(reviewer_name, {}).get('author_experience', None)
-            if reviewer_experience is not None:
-                total_reviewer_experience += reviewer_experience
-            else:
-                # Compute user data
-                author_features(pr, api, cache, diff_user=reviewer)
-                total_reviewer_experience += reviewer_cache.get(reviewer_name, {}).get('author_experience')
-
+            total_reviewer_experience += get_reviewer_experience(pr, reviewer, user_cache)
             # Review count
-            total_reviewer_review_num += reviewer_cache.get(reviewer_name, {}).get('author_review_number')
+            total_reviewer_review_num += user_cache.get(reviewer_name, {}).get('author_review_number')
 
     # Compute reviewer features
     avg_reviewer_experience = 0
@@ -77,3 +69,52 @@ def reviewer_features(pr: PullRequest, api: Github, cache: dict):
         'avg_reviewer_experience': avg_reviewer_experience,
         'avg_reviewer_review_count': avg_reviewer_review_count
     }
+
+def get_reviewer_experience(pr: PullRequest, user: NamedUser, user_cache: dict) -> float:
+    username = user.login
+
+    # Try retrieve from cache
+    experience = user_cache.get(username, {}).get('author_experience', None)
+
+    if experience is not None:
+        return experience
+
+    # Else calculate
+    registration_date = user.created_at
+    latest_revision = pr.created_at
+    experience = (latest_revision.date() - registration_date.date()).days / DAYS_PER_YEAR
+
+    # Cache result
+    if user not in user_cache:
+        user_cache[username] = {
+            'author_experience': experience
+        }
+    else:
+        user_cache[username]['author_experience'] = experience
+
+    return experience
+
+def get_reviewer_review_cnt(user: NamedUser, user_cache: dict, api: Github) -> int:
+    username = user.login
+
+    # Try retrieve from cache
+    reviews = user_cache.get(username, {}).get('author_review_number', None)
+
+    if reviews is not None:
+        return reviews
+
+    # Else calculate
+    now = datetime.now(timezone.utc)
+    start_date = now - timedelta(days=TIME_WINDOW_DAYS)
+    reviews = try_get_reviews_num(username, start_date, now, api)
+
+    # Cache result
+    if reviews is not None:
+        if user not in user_cache:
+            user_cache[username] = {
+                'author_review_number': reviews
+            }
+    else:
+        user_cache[username]['author_review_number'] = reviews
+
+    return reviews
--- src/features/user_utils.py
+++ src/features/user_utils.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from github import Github ,GithubException
 from github.NamedUser import NamedUser
 from github.PullRequest import PullRequest
@@ -8,19 +10,6 @@ def is_bot_user(username: str, repo_name: str) -> bool:
         return True
 
     return False
-
-# When trying to fetch private user data through issue search and exploring props
-# A code 422 exception is raised
-def try_get_total_prs(user: NamedUser, api: Github) -> bool:
-    try:
-        change_num = api.search_issues(f"is:pr author:{user.login}").totalCount
-
-    except GithubException as e:
-        if e.status == 422:
-            return None
-        raise e
-        
-    return change_num
 
 def is_user_reviewer(pr: PullRequest, user: NamedUser):
     if user != pr.user.login:
@@ -35,3 +24,28 @@ def is_user_reviewer(pr: PullRequest, user: NamedUser):
                 return True
 
     return False
+
+# When trying to fetch private user data through issue search and exploring props
+# A code 422 exception is raised
+def try_get_total_prs(user: NamedUser, api: Github) -> int:
+    try:
+        change_num = api.search_issues(f"is:pr author:{user.login}").totalCount
+
+    except GithubException as e:
+        if e.status == 422:
+            return None
+        raise e
+        
+    return change_num
+
+def try_get_reviews_num(username: str, start_date: datetime, end_date: datetime, api: Github) -> int:
+    try:
+        review_number = api.search_issues(f"type:pr reviewed-by:{username} closed:{start_date.date()}..{end_date.date()}").totalCount
+        review_number += api.search_issues(f"type:pr review-requested:{username} closed:{start_date.date()}..{end_date.date()}").totalCount
+
+    except GithubException as e:
+        if e.status == 422:
+            return None
+        raise e
+
+    return review_number
